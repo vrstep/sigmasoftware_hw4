1cfe6fdd10d2539f6688692f6c6a6fe3e0867e0a
Amir <amir.batyrggaliev@gmail.com>
2023-04-10 17:29:09 +0600

    Add task 4

--- script.js
+++ script.js
@@ -4,5 +4,15 @@ function stringEnd(string1, string2) {
   return string1.endsWith(string2);
 }
 
-console.log(stringEnd("abc", "bc"));
-console.log(stringEnd("abc", "d"));
+// console.log(stringEnd("abc", "bc"));
+// console.log(stringEnd("abc", "d"));
+
+// Task 4
+
+function countVowels(string) {
+  const regex = /[aeiouAEIOU]/gi;
+  return string.match(regex).length;
+}
+
+console.log(countVowels("Ahooooj"));
+console.log(countVowels("Good Morning!"));
